487db93e3cedd793befa203042021804215c0c08
zengzhen <zengzhen@si-tech.com.cn>
2019-07-11 17:11:11 +0800

    可以获取其他客户端实例信息或者本地实例信息

--- springcloud-euraka-provider/src/main/java/com/zc/controller/UserController.java
+++ springcloud-euraka-provider/src/main/java/com/zc/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.zc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.zc.entity.User;
 import com.zc.repository.UserRepository;
 
@@ -14,10 +18,25 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private EurekaClient eurekaClient;
+	
 	@GetMapping("/simple/{id}")
 	public User findById(@PathVariable Long id) {
 		return this.userRepository.findOne(id);
 	}
 
-
+	@GetMapping("/eureka-instance")
+	  public String serviceUrl() {
+	    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("SPRINGCLOUD-EUREKA-PROVIDER", false);
+	    return instance.getHomePageUrl();
+	  }
+	
+	 @Autowired
+	  private DiscoveryClient discoveryClient;
+	 @GetMapping("/instance-info")
+	  public ServiceInstance showInfo() {
+	    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+	    return localServiceInstance;
+	  }
 }
